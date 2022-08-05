cb622287722d8cdc604fbe37e735bbd928864606
festusOp7 <70211809+festusOp7@users.noreply.github.com>
2022-08-05 15:01:41 +0200

    fixade bootstrap

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,17 +1,24 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import ExercisesList from "./pages/exercises-list";
 import EditExercise from "./pages/edit-exercise";
 import CreateUser from "./pages/create-user";
 import CreateExercise from "./pages/create-exercise";
+import NavBar from "./pages/navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<ExercisesList/>}/>
-      <Route path="/users" element={<CreateUser/>}/>
-      <Route path="/edit/:id" element={<EditExercise/>}/>
-      <Route path="/create" element={<CreateExercise/>}/>
-    </Routes>
+    <BrowserRouter>
+      <NavBar/>
+      <br/>
+      <Routes>
+        <Route path="/" element={<ExercisesList/>}/>
+        <Route path="/users" element={<CreateUser/>}/>
+        <Route path="/edit/:id" element={<EditExercise/>}/>
+        <Route path="/create" element={<CreateExercise/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
